9562a8e6640cbc52a682060d3f2f0805bcbba1ae
Robbie Stamp <stamprobbie@gmail.com>
2023-08-31 12:59:53 +0100

    adding go back button

--- music-app/src/pages/Albums/index.jsx
+++ music-app/src/pages/Albums/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { default as ShowAlbums } from '../../components/ShowAlbums'
+import './style.css'
 
 const Albums = () => {
+  const navigate = useNavigate()
+
   return (
+    <>
+    <button
+        onClick={() => navigate(-1)}
+      >Go back</button>
     <ShowAlbums />
+    </>
+    
   )
 }
 
